ad59665a9e852fdf57d0965c8c035c70ba412855
dylanwatson28 <dylan_watson28@milton.edu>
2026-05-05 15:18:10 -0400

    first commit

---
+++ src/BasicGameApp.java
@@ -0,0 +1,20 @@
+public class BasicGameApp {
+    public Card[] deck;
+    public Player me;
+    public Dealer D;
+    public void compare(){
+
+    }
+    public void round(){
+
+    }
+    public void shuffle(){
+
+
+
+    }
+//change
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    public int cardTotal;
+    public boolean isBust;
+    public boolean isHit;
+    public Card[] hand;
+    public String name;
+    public void hit(){
+
+    }
+    public void stand(){
+
+    }
+}
